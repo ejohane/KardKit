bb5baee11912d19f82e9ea55d2940159a39f80c2
Ken Klatt <kenklatt@gmail.com>
2013-12-03 20:21:21 -0600

    Renamed StandardDeck's CardHolder object for clarity

--- js/StandardDeck.js
+++ js/StandardDeck.js
@@ -6,7 +6,7 @@ var Card = require('./Card.js');
 var CardHolder = require('./CardHolder.js');
 
 //var cards = [];
-var cards = new CardHolder();
+var cardHolder = new CardHolder();
 var suits = new Array("C","D","H","S");
 var n = 52;
 
@@ -21,14 +21,14 @@ for (var i = 0; i < suits.length; i++) {
 		else if (j === 13) { c.properties.rank = 'K'; }
 		else { c.properties.rank = j; }
 		//cards.push(c);
-		cards.insert(c);
+		cardHolder.insert(c);
 	}
 }
 
-/*for (var x in cards) {
+/*for (var x in cardHolder.cards) {
 	if (x % 13 === 0) { console.log('\n'); }
-	process.stdout.write(cards[x].properties['suit'] + cards[x].properties['rank'] + ' ');
+	process.stdout.write(cardHolder.cards[x].properties['suit'] + cardHolder.cards[x].properties['rank'] + ' ');
 }
 process.stdout.write("\n");*/
 
-return cards;
+return cardHolder;
