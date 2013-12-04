39ce804112a1739b921e1a95dd4f6868db22ded7
Ken Klatt <kenklatt@gmail.com>
2013-12-03 19:29:35 -0600

    StandardDeck now returns a CardHolder

--- js/StandardDeck.js
+++ js/StandardDeck.js
@@ -1,6 +1,8 @@
 var Card = require('./Card.js');
+var CardHolder = require('./CardHolder.js');
 
-var cards = [];
+//var cards = [];
+var cards = new CardHolder();
 var suits = new Array("C","D","H","S");
 var n = 52;
 
@@ -14,14 +16,15 @@ for (var i = 0; i < suits.length; i++) {
 		else if (j === 12) { c.properties.rank = 'Q'; }
 		else if (j === 13) { c.properties.rank = 'K'; }
 		else { c.properties.rank = j; }
-		cards.push(c);
+		//cards.push(c);
+		cards.insert(c);
 	}
 }
 
-for (var x in cards) {
+/*for (var x in cards) {
 	if (x % 13 === 0) { console.log('\n'); }
 	process.stdout.write(cards[x].properties['suit'] + cards[x].properties['rank'] + ' ');
 }
-process.stdout.write("\n");
+process.stdout.write("\n");*/
 
 return cards;
