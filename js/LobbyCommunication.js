4c622e6fbd65fbf57fb87c08b4b3a16eb7c5c386
ErikJohansson <ejohane@gmail.com>
2013-12-08 16:01:16 -0600

    craigs stuff

--- js/LobbyCommunication.js
+++ js/LobbyCommunication.js
@@ -311,7 +311,7 @@ LobbyCommunication.prototype.getGameRoomList = function(){
     Updated:
 *********************************************************************/
 LobbyCommunication.prototype.playerDisconnected = function(playerID){
-    var player = this.players[playerID];
+    var player = this.getPlayersByID(playerID);
     //remove player from room
     var room = player.room;
     if(room != undefined){
