852c71f0ad4d62f1b891c69b8a53b17a2268c922
sniprpenguin <bdjaberg@uwm.edu>
2013-12-09 14:44:02 -0500

    Modified hand.js

--- js/hand.js
+++ js/hand.js
@@ -9,7 +9,10 @@ util.inherits(Hand, CardHolder);
 
 //takes a card of choice and places it in the destination holder
 Hand.super_.prototype.play = function(index, destination) {
-	var temp = this.cards.splice(index, 1);
+	var temp = this.cards[0];
+	for (int i = 1; i < this.numCards; i++){
+		this.cards[i-1] = this.cards[i];
+	}
 	this.numCards = this.numCards - 1;
 	destination.insert(temp);
 }
